949503054e46a76c7c5b3432c796cf9735b801ae
Charles Mariot <contact@charles-mariot.fr>
2021-03-26 16:04:08 +0100

    C07

---
+++ C07/ex01/ft_range.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmariot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/26 15:45:44 by cmariot           #+#    #+#             */
+/*   Updated: 2021/03/26 16:03:40 by cmariot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		*ft_range(int min, int max)
+{
+	int range;
+	int *tab;
+	int i;
+
+	if (min >= max)
+		return NULL;
+	range = max - min;
+	if (!(tab = (int *)malloc(sizeof(*tab) * range)))
+		return NULL;
+	i = 0;
+	while (i < range)
+	{
+		tab[i] = min + i;
+		i++;
+	}
+	return (tab);
+}
+
+int		main(void)
+{
+	int min;
+	int max;
+	int *tab;
+
+	min = 3;
+	max = 6;
+	ft_range(min, max);
+	free(tab);
+	return (0);
+}

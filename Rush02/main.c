6e9e07b3059113e7ad5f2dad4292fbba9f0cbfbf
Charles Mariot <contact@charles-mariot.fr>
2021-03-28 08:23:47 +0200

    backup

--- Rush02/main.c
+++ Rush02/main.c
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: charles <charles@student.42.fr>            +#+  +:+       +#+        */
+/*   By: cmariot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/26 21:12:26 by charles           #+#    #+#             */
-/*   Updated: 2021/03/26 23:57:09 by charles          ###   ########.fr       */
+/*   Created: 2021/03/27 10:18:10 by cmariot           #+#    #+#             */
+/*   Updated: 2021/03/27 12:46:25 by cmariot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+#define	BUF_SIZE	691
 
 void    ft_putchar(char c)
 {
@@ -42,71 +45,84 @@ int     ft_strlen(char *str)
     return (i);
 }
 
-int		ft_isspace(char c)
+unsigned long long	ft_atoi(char *str)
 {
-	if ((c == '\t' || c == '\n' || c == '\v') ||
-		(c == '\f' || c == '\r' || c == 32))
-		return (1);
-	else
-		return (0);
-}
+	unsigned int i;
+	unsigned long long nb;
+	unsigned int str_len;
 
-int		ft_count_minus(char c)
-{
-	if (c == '-' || c == '+')
-		return (1);
-	else
-		return (0);
-}
-
-unsigned int	ft_atoi(char *str)
-{
-	int i;
-	unsigned int nb;
-
+	str_len = ft_strlen(str);
 	i = 0;
-    nb = 0;
+	nb = 0;
     while (str[i] >= '0' && str[i] <= '9')
     {
-        nb = nb * 10 + (str[i] - '0');
-        i++;
-    }
-	return (nb);
+       	nb = nb * 10 + (str[i] - '0');
+       	i++;
+	}
+	if (str_len == i)
+		return (nb);
+	else
+		return (9999999999);
 }
 
-// [0 ; 65535]
+// [0 ; 4294967295]
 int     ft_is_it_a_validate_unsigned_int(char *str)
 {
-    unsigned int    nbr;
+    unsigned long long    nbr;
 
     nbr = ft_atoi(str);
-    if (nbr >= 0 && nbr <= 65535 && i <= 5)
+    if (nbr >= 0 && nbr <= 4294967295)
+        return (1);
+	else
+		return (0);
+}
+
+int		open_dictionary()
+{
+	int		file_descriptor;
+	char	buf[BUF_SIZE + 1];
+	int 	read_return;
+	int		close_descriptor;
+
+	file_descriptor = open("./dictionary.txt", O_RDWR, O_APPEND);
+    if (file_descriptor == -1)
     {
+	    ft_putstr("Error : open dictionary failed\n");
         return (1);
     }
-    else
+    printf("File Descriptor : %d\n", file_descriptor);
+    read_return = read(file_descriptor, buf, BUF_SIZE);
+    buf[read_return] = '\0';
+    printf("taille buffer = %d\n", read_return);
+    ft_putstr(buf);
+
+
+    close_descriptor = close(file_descriptor);
+    if (close_descriptor == -1)
     {
-        return (0);
-    }
+	    ft_putstr("Error : close dictionary failed\n");
+		return (1);
+    }   
+	return (0);
 }
 
 int     main(int argc, char **argv)
 {
-    char *reference_dictionary;
-    char *value_to_convert;
+    char 	*reference_dictionary;
+    char 	*value_to_convert;
 
         if (argc == 2)
         {
             value_to_convert = argv[1];
             if (ft_is_it_a_validate_unsigned_int(value_to_convert) == 1)
             {
-                printf("%s\n", argv[1]);
-                //parse(argv[1]);
-            }
+                printf("1 argument, valide ui : %s\n", argv[1]);
+            	open_dictionary();
+			}
             else
             {
-                ft_putstr("Error");
-                ft_putchar('\n');
+                ft_putstr("Error : non valide unsigned int\n");
+				return (1);
             }
             return (0);
         }
@@ -114,15 +130,18 @@ int     main(int argc, char **argv)
         {
             reference_dictionary = argv[1];
             value_to_convert = argv[2];
-            if (ft_is_it_a_validate_unsigned_int(value_to_convert) == 1)
+            if (ft_is_it_a_validate_unsigned_int(value_to_convert) == 1 )
             {
-                printf("%s\n", argv[1]);
-                printf("%s\n", argv[2]);
+                printf("2 arguments, dictionary %s\n", argv[1]);
+                printf("2 arguments, valide ui %s\n", argv[2]);
                 //parse(argv[2]);
             }
-            return (0);
+			else
+			{
+                ft_putstr("Error\n");
+				return (1);
+			}
         }
-    ft_putstr("Error");
-    ft_putchar('\n');
+    ft_putstr("Error\n");
     return (0);
 }
